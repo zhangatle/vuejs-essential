ed9a34a27864075adda83308bfad034d23ad2b5e
张乐 <zhangle@le.lan>
2019-12-10 22:47:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  h1 {
+    color: #42b983;
+  }
+}
+</style>
