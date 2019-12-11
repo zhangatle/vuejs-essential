725edf8a2a524e12582bee1b731af31a3789385a
zhangle <zhangle@wud360.com>
2019-12-11 11:09:08 +0800

    添加注册页面并引入路由

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -8,10 +8,10 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a href="/" class="navbar-brand">
+        <router-link to="/" class="navbar-brand">
           <span class="title">{{logo.title}}</span>
           <img :src="logo.src" :alt="logo.title">
-        </a>
+        </router-link>
       </div>
       <div id="top-navbar-collapse" :class="['collapse','navbar-collapse',{in:showCollapseNav}]">
         <ul class="nav navbar-nav">
@@ -19,14 +19,20 @@
             <a href="#" @click="changeNavIndex(index)">{{item}}</a>
           </li>
         </ul>
+        <div class="navbar-right">
+          <TheEntry/>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+    import TheEntry from "@/components/layouts/TheEntry"
     export default {
         name: "TheHeader",
+      components: {TheEntry},
+      comments: 'TheEntry',
         data(){
           return {
             logo: {
